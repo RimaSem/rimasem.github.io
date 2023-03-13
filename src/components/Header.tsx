8c3aa4ca4def3b42672d9eb1f099a00c686a1df3
RimaSem <rima.semetaite@gmail.com>
2023-03-13 17:54:19 +0200

    Style header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,35 @@
 import "./scss/Header.scss";
 import { Link } from "react-router-dom";
+import { useRef } from "react";
 
 function Header() {
+  const hamburgerIconRef = useRef<HTMLDivElement>(null);
+  const slidingMenuRef = useRef<HTMLDivElement>(null);
+
+  function displayMenu() {
+    hamburgerIconRef.current?.classList.toggle("active");
+    slidingMenuRef.current?.classList.toggle("active");
+  }
+
   return (
     <header>
       <div className="header-container">
-        <h3>{"<Rima Šemetaitė />"}</h3>
-        <nav>
+        <h3>
+          {"<Rima "}
+          <span className="surname-span">Šemetaitė </span>
+          {"/>"}
+        </h3>
+        <div ref={slidingMenuRef} className="sliding-menu"></div>
+        <div
+          ref={hamburgerIconRef}
+          className="hamburger-icon"
+          onClick={displayMenu}
+        >
+          <div className="ham-bar bar-top"></div>
+          <div className="ham-bar bar-mid"></div>
+          <div className="ham-bar bar-bottom"></div>
+        </div>
+        <nav className="wide-screen-nav">
           <a id="about-link" href="#">
             About
           </a>
