a808d2ff1639cdb515d692be11c790d9e0d16689
RimaSem <rima.semetaite@gmail.com>
2023-03-16 18:06:27 +0200

    Create contact section

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,6 +14,7 @@ function App() {
       <Hero />
       <About />
       <Projects />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/Contact.tsx
@@ -0,0 +1,68 @@
+import "./scss/Contact.scss";
+import { Icon } from "@mdi/react";
+import {
+  mdiCellphone,
+  mdiEmail,
+  mdiFacebook,
+  mdiGithub,
+  mdiLinkedin,
+  mdiMapMarker,
+} from "@mdi/js";
+
+function Contact() {
+  return (
+    <>
+      <h1>CONTACT ME</h1>
+      <section className="contact-container">
+        <form>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Name"
+            aria-label="Name"
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Your Email"
+            aria-label="Email"
+          />
+          <textarea
+            name="message"
+            id="message"
+            placeholder="Message"
+            aria-label="Message"
+          />
+          <button type="button">Send Message</button>
+        </form>
+        <div className="contacts">
+          <div className="contact-info">
+            <h3>Contact Info:</h3>
+            <div className="wrapper">
+              <Icon className="contact-icon" path={mdiEmail} />
+              <p>rima.semetaite@gmail.com</p>
+            </div>
+            <div className="wrapper">
+              <Icon className="contact-icon" path={mdiCellphone} />
+              <p>+370 608 00903</p>
+            </div>
+            <div className="wrapper">
+              <Icon className="contact-icon" path={mdiMapMarker} />
+              <p>Klaipėda, Lithuania</p>
+            </div>
+          </div>
+          <div className="social-networks">
+            <h3>Social Networks:</h3>
+            <div className="icons">
+              <Icon className="social-icon" path={mdiGithub} size={1.3} />
+              <Icon className="social-icon" path={mdiFacebook} size={1.3} />
+              <Icon className="social-icon" path={mdiLinkedin} size={1.3} />
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Contact;
